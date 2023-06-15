4d1fff7b0f2988263ee03aa69a393715ebf541c3
tonylim89 <tonylim9@hotmail.com>
2023-06-15 20:12:39 +0800

    Initial Springboot Project

---
+++ src/main/java/sg/edu/nus/iss/demo_springboot_interceptors/DemoSpringbootInterceptorsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.demo_springboot_interceptors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootInterceptorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootInterceptorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/demo_springboot_interceptors/DemoSpringbootInterceptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.demo_springboot_interceptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootInterceptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
